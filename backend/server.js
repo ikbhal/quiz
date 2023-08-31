08db73f20e9d48d47e2b191a474eb906e7097962
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2023-08-31 13:00:07 +0530

    cors support

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const cors = require('cors');
 
 const app = express();
 const PORT = 5001;
+
+
+// Enable CORS
+app.use(cors());
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
